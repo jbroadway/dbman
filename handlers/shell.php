49ea2dfed760f3791aae80b55ba6e4093c7af797
Johnny Broadway <johnny@johnnybroadway.com>
2012-03-09 14:36:25 -0600

    SQL shell now supports multiple queries, better handles output, and clarifies output for CRUD vs select operations, closes #7

--- handlers/shell.php
+++ handlers/shell.php
@@ -10,41 +10,70 @@ if (! User::require_admin ()) {
 $page->title = i18n_get ('SQL Shell');
 
 if (isset ($_GET['query']) && ! empty ($_GET['query'])) {
-	$res = db_fetch_array ($_GET['query']);
+	$queries = explode (';', $_GET['query']);
 } else {
-	$res = array ();
+	$queries = array ();
 }
+$res = array ();
 
-if (count ($res) > 0) {
-	$headers = array_keys ((array) $res[0]);
-} else {
-	$headers = array ();
+foreach ($queries as $query) {
+	$query = trim ($query);
+	if ($query === '') {
+		continue;
+	}
+	$exec = preg_match ('/^(alter|create|insert|update|delete) /i', $query);
+	if ($exec) {
+		$res[$query] = array (
+			'headers' => array (),
+			'results' => db_execute ($query),
+			'error' => false,
+			'exec' => $exec
+		);
+	} else {
+		$res[$query] = array (
+			'headers' => array (),
+			'results' => db_fetch_array ($query),
+			'error' => false,
+			'exec' => $exec
+		);
+	}
+	if ($res[$query]['results'] === false) {
+		$res[$query]['error'] = db_error ();
+	} elseif (count ($res[$query]['results']) > 0) {
+		$res[$query]['headers'] = array_keys ((array) $res[$query]['results'][0]);
+	}
 }
 
 echo $tpl->render ('dbman/shell', array ('query' => $_GET['query']));
 
-if (isset ($_GET['query'])) {
-	$err = db_error ();
-	if ($err) {
-		echo '<p>' . i18n_get ('Error') . ': ' . $err . "</p>\n";
-	} else {
-		echo '<p>' . count ($res) . ' ' . i18n_get ('results') . ":</p>\n";
-	
-		
-		echo "<p><table width='100%'><tr>\n";
-		foreach ($headers as $header) {
-			printf ("<th>%s</th>\n", $header);
+foreach ($res as $query => $info) {
+	echo '<h5><pre>' . Template::sanitize ($query) . "</pre></h5>\n";
+
+	if ($info['error']) {
+		printf ("<p>%s: %s</p>\n", i18n_get ('Error'), $info['error']);
+		continue;
+	}
+
+	if ($info['exec']) {
+		printf ("<p>%s</p>\n", i18n_get ('Query executed.'));
+		continue;
+	}
+
+	printf ("<p>%d %s:</p>\n", count ($info['results']), i18n_get ('results'));
+
+	echo "<p><table width='100%'><tr>\n";
+	foreach ($info['headers'] as $header) {
+		printf ("<th>%s</th>\n", $header);
+	}
+	echo "</tr>\n";
+	foreach ($info['results'] as $row) {
+		echo "<tr>\n";
+		foreach ((array) $row as $k => $v) {
+			printf ("<td>%s</td>\n", Template::sanitize ($v));
 		}
 		echo "</tr>\n";
-		foreach ($res as $row) {
-			echo "<tr>\n";
-			foreach ((array) $row as $k => $v) {
-				printf ("<td>%s</td>\n", $v);
-			}
-			echo "</tr>\n";
-		}
-		echo "</table></p>\n";
 	}
+	echo "</table></p>\n";
 }
 
 ?>
